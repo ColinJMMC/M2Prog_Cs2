e4414e59dac2d96049f2bcb349d9db21f916bbec
ColinJMMC <colin.ivko@gmail.com>
2024-11-22 12:32:03 +0100

    vragen

--- 01 Functions/01 Functions/Program.cs
+++ 01 Functions/01 Functions/Program.cs
@@ -7,14 +7,77 @@
         {
             {
                 Program program = new Program(); // Variable om de program the runnen
-                program.Run(); // Het is een progam function en runt ons programma
+                program.vraag1();
+                program.Vraag2();
+                program.Vraag3();
+                program.Vraag4();
+                program.Vraag5();
+                program.Vraag6();
+                program.Vraag7();
+
+
+
             }
         }
 
         // Function hier
-        internal void Run() {
-            Console.WriteLine("Dit is nu de start van mijn programma");
+        internal void vraag1()
+        {
+            Console.WriteLine("How much money do you have?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
         }
+
+        internal void Vraag2()
+        {
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag3()
+        {
+            Console.WriteLine("What secret conspiracy would you like to actually start letting other people know?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag4()
+        {
+            Console.WriteLine("If you can still remember, what are your funniest childhood memories?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag5()
+        {
+            Console.WriteLine("What would you do if you won a million dollars?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag6()
+        {
+            Console.WriteLine("What is the worst game you played but that you liked anyway?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag7()
+        {
+            Console.WriteLine("Why do round pizzas come in square boxes?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+
 
     }
 }
