72f66eb7fc7ea7ddc8a60e6ba577fd972c7fe160
ColinJMMC <colin.ivko@gmail.com>
2024-12-13 12:19:47 +0100

    classes

--- 01 Functions/01 Functions/Program.cs
+++ 01 Functions/01 Functions/Program.cs
@@ -2,82 +2,116 @@
 {
     internal class Program
     {
+        string[] vragen = new string[]
+        {
+            "Skyrim is the fifth installment of what epic open-world videogame series by Bethesda Softworks?",
+            "Which Legend of Zelda game that picks up after Ocarina of Time was released for N64 in 2000 and remade for Nintendo 3DS in 2015?",
+            "What arcade game was called Puckman in Japan?",
+            "In July 2023, video game company EA announced that players will be able to explore Wakanda in an upcoming open-world video game based on the adventures of what Marvel superhero?\r\n",
+            "Which spooky 2001 GameCube game starring Mario’s brother got a reboot for Nintendo Switch in 2019?",
+            "In May 2022, the government of what European nation banned its employees from using American gaming terms such as \"e-sports,\" instead using their domestic language counterparts like \"jeu video de competition?\"\r\n",
+            "Regarded as one of the greatest video games of all time, what 1981 arcade game features the titular amphibian trying to cross a road without getting squashed by cars, and a river without getting eaten by predators?\r\n",
+            "What is thought to be the first video game, created in 1958 and becoming popular in the 1970s?",
+            "What Will Wright created video game series released in 2000 (with sequels in 2004, 2009, and 2014) saw players watching and directing characters to mundane things like eating, sleeping, and cleaning their houses?\r\n",
+            "What 1997 N64 video game, features James Bond and is named after the 1995 film?"
+        };
+
 
         static void Main(string[] args) // void, er komt niks terug
         {
             {
                 Program program = new Program(); // Variable om de program the runnen
-                program.vraag1();
-                program.Vraag2();
-                program.Vraag3();
-                program.Vraag4();
-                program.Vraag5();
-                program.Vraag6();
-                program.Vraag7();
-
-
-
+                program.Run();
+                
             }
         }
 
         // Function hier
-        internal void vraag1()
+        internal void Run()
         {
-            Console.WriteLine("How much money do you have?");
-            string antwoord = Console.ReadLine();
+            string vraag0 = GetVraag(GetRandomVraag);
+            Console.WriteLine(vraag0);
 
-            Console.WriteLine(antwoord);
+            //Console.WriteLine("Why do round pizzas come in square boxes?");
+            //string antwoord = Console.ReadLine();
+
+            //Console.WriteLine(antwoord);
+
+            //vraag1();
+            //Vraag2();
+            //Vraag3();
+            //Vraag4();
+            //Vraag5();
+            //Vraag6();
+            //string antwoord2 = Vraag7();
+            //Console.WriteLine(antwoord2);
         }
 
-        internal void Vraag2()
-        {
-            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
-            string antwoord = Console.ReadLine();
-
-            Console.WriteLine(antwoord);
+        internal string GetVraag(int vraagIndex) {
+            return vragen[vraagIndex];
         }
 
-        internal void Vraag3()
+        internal string GetRandomVraag()
         {
-            Console.WriteLine("What secret conspiracy would you like to actually start letting other people know?");
-            string antwoord = Console.ReadLine();
-
-            Console.WriteLine(antwoord);
+            int random = 0, 9;
+            return GetVraag(random);
         }
 
-        internal void Vraag4()
-        {
-            Console.WriteLine("If you can still remember, what are your funniest childhood memories?");
-            string antwoord = Console.ReadLine();
+    //    internal void vraag1()
+    //    {
+    //        Console.WriteLine("How much money do you have?");
+    //        string antwoord = Console.ReadLine();
 
-            Console.WriteLine(antwoord);
+    //        Console.WriteLine(antwoord);
+    //    }
+
+    //    internal void Vraag2()
+    //    {
+    //        Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+    //        string antwoord = Console.ReadLine();
+
+    //        Console.WriteLine(antwoord);
+    //    }
+
+    //    internal void Vraag3()
+    //    {
+    //        Console.WriteLine("What secret conspiracy would you like to actually start letting other people know?");
+    //        string antwoord = Console.ReadLine();
+
+    //        Console.WriteLine(antwoord);
+    //    }
+
+    //    internal void Vraag4()
+    //    {
+    //        Console.WriteLine("If you can still remember, what are your funniest childhood memories?");
+    //        string antwoord = Console.ReadLine();
+
+    //        Console.WriteLine(antwoord);
+    //    }
+
+    //    internal void Vraag5()
+    //    {
+    //        Console.WriteLine("What would you do if you won a million dollars?");
+    //        string antwoord = Console.ReadLine();
+
+    //        Console.WriteLine(antwoord);
+    //    }
+
+    //    internal void Vraag6()
+    //    {
+    //        Console.WriteLine("What is the worst game you played but that you liked anyway?");
+    //        string antwoord = Console.ReadLine();
+
+    //        Console.WriteLine(antwoord);
+    //    }
+
+    //    internal string Vraag7()
+    //    {
+    //        Console.WriteLine("How many kids do you have?");
+    //        string antwoord = Console.ReadLine();
+
+    //        return antwoord;
+    //    }
+
         }
-
-        internal void Vraag5()
-        {
-            Console.WriteLine("What would you do if you won a million dollars?");
-            string antwoord = Console.ReadLine();
-
-            Console.WriteLine(antwoord);
-        }
-
-        internal void Vraag6()
-        {
-            Console.WriteLine("What is the worst game you played but that you liked anyway?");
-            string antwoord = Console.ReadLine();
-
-            Console.WriteLine(antwoord);
-        }
-
-        internal void Vraag7()
-        {
-            Console.WriteLine("Why do round pizzas come in square boxes?");
-            string antwoord = Console.ReadLine();
-
-            Console.WriteLine(antwoord);
-        }
-
-
-
     }
-}
---
+++ 02 Classes/RijSchool/RijSchool/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RijSchool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        class Auto
+        {
+            string color = "red";
+        }
+    }
+}
